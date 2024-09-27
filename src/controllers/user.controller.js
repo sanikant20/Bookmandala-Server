0f85093cef8ba1af40844a877186057d2391596e
Sanikant Kushwaha <sanikant508@gmail.com>
2024-09-27 15:29:16 +0545

    add user APIs for changePassword, editData, update avatar, and logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -22,23 +22,14 @@ const generateRefreshTokens = async (userId) => {
 const options = {
     httpOnly: true,
     secure: true,
-    sameSite: "Strict",
-    maxAge: 7 * 24 * 60 * 60 * 1000, // Cookie expires in 7 days,
-    path: "/",
-    domain: process.env.CORS_ORIGIN,
+    // sameSite: "Strict",
+    // maxAge: 7 * 24 * 60 * 60 * 1000, // Cookie expires in 7 days,
+    // path: "/",
+    // domain: process.env.CORS_ORIGIN,
 }
 
 // Register User Controller
 const register = asyncHandler(async (req, res) => {
-    // take data from body
-    // validate data
-    // Check existing user
-    // upload avatar
-    // validate avatar
-    // create new user
-    // validate new user
-    // return respose
-
     const { fullname, email, phoneNumber, dob, gender, password } = req.body
 
     if (
@@ -120,6 +111,35 @@ const login = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, { user: loggedInUser, "refreshToken": refreshToken }, "Login successfully"))
 })
 
+// logout Controller
+const logout = asyncHandler(async (req, res) => {
+    const { _id: userId } = req.user
+    if (!userId) {
+        throw new ApiError(400, "User is not logged in.")
+    }
+
+    const user = await User.findByIdAndUpdate(userId,
+        {
+            $unset: {
+                refreshToken: 1
+            }
+        },
+        {
+            new: true
+        }
+    )
+    if (!user) {
+        throw new ApiError(500, "Something went wrong while logging out.")
+    }
+
+    return res
+        .status(200)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "Logout successfully"))
+
+})
+
+// Get Current User Controller
 const getCurrentUser = asyncHandler(async (req, res) => {
     const { _id: userId } = req.user
     if (!userId) {
@@ -132,11 +152,113 @@ const getCurrentUser = asyncHandler(async (req, res) => {
     }
 
     return res.status(200).json(new ApiResponse(200, user, "User retrieved successfully."))
-
 })
 
+// Edit User Data Controller
+const editUserData = asyncHandler(async (req, res) => {
+    const { _id: userId } = req.user
+    if (!userId) {
+        throw new ApiError(400, "User is not logged in.")
+    }
+
+    const { fullname, phoneNumber, dob, gender, shippingAddress } = req.body
+
+    const user = await User.findByIdAndUpdate(userId,
+        {
+            $set: {
+                fullname: fullname,
+                phoneNumber: phoneNumber,
+                dob: dob,
+                gender: gender,
+                shippingAddress: shippingAddress
+            }
+        },
+        {
+            new: true
+        }
+    ).select("-password -email")
+    if (!user) {
+        throw new ApiError(500, "Something went wrong while updating user data.")
+    }
+
+    return res.status(200).json(new ApiResponse(200, user, "User data updated successfully."))
+})
+
+// Update Avatar Controller
+const updateAvatar = asyncHandler(async (req, res) => {
+    const { _id: userId } = req.user
+    if (!userId) {
+        throw new ApiError(400, "User is not logged in.")
+    }
+
+    const avatarLocalFilepath = await req.file?.path
+    if (!avatarLocalFilepath) {
+        throw new ApiError(400, "Avatar is required for updating avatar.")
+    }
+
+    const cloudAvatar = await uploadOnCloudinary(avatarLocalFilepath)
+    if (!cloudAvatar) {
+        throw new ApiError(400, "Failed to upload avatar on cloudinary.")
+    }
+
+    const user = await User.findByIdAndUpdate(userId,
+        {
+            $set: {
+                avatar: cloudAvatar.url
+            }
+        },
+        {
+            new: true
+        }
+    )
+    if (!user) {
+        throw new ApiError(500, "Something went wrong while updating avatar.")
+    }
+
+    return res.status(200).json(new ApiResponse(200, user, "Avatar updated successfully."))
+})
+
+// Change Password Controller
+const changePassword = asyncHandler(async (req, res) => {
+    const { oldPassword, newPassword, confirmNewPassword } = req.body
+    if (!(oldPassword && newPassword && confirmNewPassword)) {
+        throw new ApiError(400, "All fields are required.")
+    }
+    if (newPassword !== confirmNewPassword) {
+        throw new ApiError(400, "newPassword and confirmNewPassword does not match.")
+    }
+
+    const { _id: userId } = req.user
+    if (!userId) {
+        throw new ApiError(400, "User is not logged in.")
+    }
+
+
+    const user = await User.findById(userId)
+    if (!user) {
+        throw new ApiError(404, "User not found.")
+    }
+
+    const isOldPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+    if (!isOldPasswordCorrect) {
+        throw new ApiError(400, "Old password is incorrect.")
+    }
+
+    user.password = newPassword
+    await user.save({ validateBeforeSave: false })
+
+    return res.status(200).json(new ApiResponse(200, user, "Password updated successfully."))
+})
+
+
+
+// Export Controllers
 export {
     register,
     login,
-    getCurrentUser
+    logout,
+    getCurrentUser,
+    editUserData,
+    updateAvatar,
+    changePassword
 }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import {
+    changePassword,
+    editUserData,
     getCurrentUser,
     login,
+    logout,
     register,
+    updateAvatar,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyToken } from "../middlewares/auth.middleware.js";
@@ -21,7 +25,10 @@ router.route('/register').post(
 router.route("/login").post(login)
 
 // Secure Route
-router.use(verifyToken)
-router.route("/get-user").get(getCurrentUser)
+router.route("/logout").post(verifyToken, logout)
+router.route("/get-user").get(verifyToken, getCurrentUser)
+router.route("/edit-user-data").patch(verifyToken, editUserData)
+router.route("/update-avatar").patch(verifyToken, upload.single("avatar"), updateAvatar)
+router.route("/change-password").patch(verifyToken, changePassword)
 
 export default router
