2ec89b8ca66e9a3b65b02e53c95610fb902880de
Sanikant Kushwaha <sanikant508@gmail.com>
2024-10-06 13:28:45 +0545

    added timestamp for cart

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -1,27 +1,31 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const cartSchema = new Schema({
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    books: [
-        {
-            bookId: {
-                type: Schema.Types.ObjectId,
-                ref: "Book",
-                required: true
-            },
-            quantity: {
-                type: Number,
-                required: true
+const cartSchema = new Schema(
+    {
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        books: [
+            {
+                bookId: {
+                    type: Schema.Types.ObjectId,
+                    ref: "Book",
+                    required: true
+                },
+                quantity: {
+                    type: Number,
+                    required: true
+                }
             }
+        ],
+        total: {
+            type: Number
         }
-    ],
-    total: {
-        type: Number
-    }
-});
+    },
+    {
+        timestamps: true
+    });
 
 export const Cart = mongoose.model("Cart", cartSchema);
